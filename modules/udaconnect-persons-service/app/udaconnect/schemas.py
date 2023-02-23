e12965b9844ac148ee38cfd8e899c769640bb8e4
ShehabEl-DeenAlalkamy <essamshehab33_3@h-eng.helwan.edu.eg>
2023-02-23 18:35:20 +0200

    feat: retain udaconnect-persons-service app.udaconnect.schemas

--- modules/udaconnect-persons-service/app/udaconnect/schemas.py
+++ modules/udaconnect-persons-service/app/udaconnect/schemas.py
@@ -1,6 +1,17 @@
-from app.udaconnect.models import Person
+from app.udaconnect.models import Person, Location
 
 from marshmallow import Schema, fields
+
+
+class LocationSchema(Schema):
+    id = fields.Integer()
+    person_id = fields.Integer()
+    longitude = fields.String(attribute="longitude")
+    latitude = fields.String(attribute="latitude")
+    creation_time = fields.DateTime()
+
+    class Meta:
+        model = Location
 
 
 class PersonSchema(Schema):
@@ -11,3 +22,8 @@ class PersonSchema(Schema):
 
     class Meta:
         model = Person
+
+
+class ConnectionSchema(Schema):
+    location = fields.Nested(LocationSchema)
+    person = fields.Nested(PersonSchema)
