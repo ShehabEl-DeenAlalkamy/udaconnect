1972444e0838f74aedb8b06769c95f756dd73dd5
ShehabEl-DeenAlalkamy <essamshehab33_3@h-eng.helwan.edu.eg>
2023-02-23 18:14:29 +0200

    feat: replaced logger.exception with logger.error for clean logging

--- modules/udaconnect-persons-service/app/udaconnect/services.py
+++ modules/udaconnect-persons-service/app/udaconnect/services.py
@@ -29,22 +29,22 @@ class PersonService:
             db.session.commit()
         except IntegrityError as e:
             if isinstance(e.orig, UniqueViolation):
-                _logger.exception(
+                _logger.error(
                     "IntegrityError: a Person with the same id already exists")
                 error = {
                     "status_code": 400,
                     "message": f"Bad Request: retry again may solve the problem"
                 }
             else:
-                _logger.exception(
-                    f"IntegrityError: unable to create 'Person' reason={str(e)}")
+                _logger.error(
+                    f"IntegrityError: unable to create 'Person' reason=\"{str(e)}\"")
                 error = {
                     'status_code': 500,
                     'message': "The server encountered an internal error and was unable to complete your request. Either the server is overloaded or there is an error in the application."
                 }
         except Exception as e:
-            _logger.exception(
-                f"error: unable to create 'Person' reason={str(e)}")
+            _logger.error(
+                f"error: unable to create 'Person' reason=\"{str(e)}\"")
             error = {
                 'status_code': 500,
                 'message': "The server encountered an internal error and was unable to complete your request. Either the server is overloaded or there is an error in the application."
@@ -60,22 +60,22 @@ class PersonService:
             if bool(person):
                 _logger.info(f"found person={PersonSchema().dump(person)}")
             else:
-                _logger.exception(
+                _logger.error(
                     f"error: NoResultFound for person_id={person_id}")
                 error = {
                     'status_code': 404,
                     'message': "Resource not found."
                 }
         except exc.NoResultFound:
-            _logger.exception(
+            _logger.error(
                 f"error: NoResultFound for person_id={person_id}")
             error = {
                 'status_code': 404,
                 'message': "Resource not found."
             }
         except Exception as e:
-            _logger.exception(
-                f"error: unable to fetch person_id={person_id} reason={str(e)}")
+            _logger.error(
+                f"error: unable to fetch person_id={person_id} reason=\"{str(e)}\"")
             error = {
                 'status_code': 500,
                 'message': "The server encountered an internal error and was unable to complete your request. Either the server is overloaded or there is an error in the application."
@@ -89,8 +89,8 @@ class PersonService:
         try:
             persons = db.session.query(Person).all()
         except Exception as e:
-            _logger.exception(
-                f"error: unable to fetch persons reason={str(e)}")
+            _logger.error(
+                f"error: unable to fetch persons reason=\"{str(e)}\"")
             error = {
                 'status_code': 500,
                 'message': "The server encountered an internal error and was unable to complete your request. Either the server is overloaded or there is an error in the application."
