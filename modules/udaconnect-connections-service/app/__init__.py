2149d96b921ecabb2dfd1dcb3265f468ccb845b2
ShehabEl-DeenAlalkamy <essamshehab33_3@h-eng.helwan.edu.eg>
2023-02-24 18:55:15 +0200

    feat: rename grpc.client to grpc.clients for future clients

--- modules/udaconnect-connections-service/app/__init__.py
+++ modules/udaconnect-connections-service/app/__init__.py
@@ -13,7 +13,7 @@ db = SQLAlchemy()
 
 
 def create_app(env=None):
-    from app.udaconnect.grpc.client import PersonStub
+    from app.udaconnect.grpc.clients import PersonStub
     from app.config import config_by_name
     from app.routes import register_routes
 
