da105a9d5b43e9b3925bf108a0b2c22c9c2a6532
ShehabEl-DeenAlalkamy <essamshehab33_3@h-eng.helwan.edu.eg>
2023-02-24 16:39:13 +0200

    feat: add grpc.services.person_service module

---
+++ modules/udaconnect-persons-service/app/udaconnect/grpc/services/person_service.py
@@ -0,0 +1,25 @@
+from app.udaconnect.grpc.proto import person_pb2_grpc as pb2_grpc
+from app.udaconnect.grpc.proto import person_pb2 as pb2
+
+
+class PersonService(pb2_grpc.PersonServiceServicer):
+    def __init__(self, app):
+        self.app = app
+        pb2_grpc.PersonServiceServicer.__init__(self)
+
+    def ListPersons(self, request, context):
+        with self.app.context():
+            from app.udaconnect import services as app_services
+            from app.config import _init_logger
+            import logging
+
+            _init_logger
+            logger = logging.getLogger(
+                'udaconnect.persons_svc.grpc.server.person_svc')
+
+            persons, _ = app_services.PersonService.retrieve_all()
+
+            logger.info(f"received {len(persons)} persons")
+            logger.info(f"sending back to client..")
+
+            return pb2.PersonMessageList(**persons)
